63bfda1ad4c24fd0e23f38ae71a8829600793e45
minzhiwei <minzhiwei@kjtpay.com.cn>
2018-12-21 15:49:29 +0800

    服务注册

--- src/main/java/com/min/spring/cloud/eureka/client/App.java
+++ src/main/java/com/min/spring/cloud/eureka/client/App.java
@@ -1,8 +1,13 @@
 package com.min.spring.cloud.eureka.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Create By minzhiwei On 2018/12/21 14:39
@@ -10,7 +15,20 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
+@EnableDiscoveryClient
 public class App {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
